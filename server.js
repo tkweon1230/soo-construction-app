07d34bcb42dd166b5184ef1197571df6c00fc721
tkweon1230 <taehkweon@gmail.com>
2023-05-03 19:00:22 -0500

    finished db set up

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -21,6 +23,9 @@ db.once('open', (error) => console.log('Connected to DB'))
 //const homeRouter = require('./routes/home')
 //app.use('/', homeRouter)
 const indexRouter = require('./routes/index')
+const contactRouter = require('./routes/contact')
+
 app.use('/', indexRouter)
+app.use('/contact', contactRouter)
 
 app.listen(process.env.PORT || 3000)
